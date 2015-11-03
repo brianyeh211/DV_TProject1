15d7f47b136adb3bb2b1d05e8889c31d5bfae1f6
unknown <brianyeh211@gmail.com>
2015-11-02 21:02:28 -0600

    Scatterplot and barchart done

---
+++ 00 Doc/Titanic_Analysis.md
@@ -0,0 +1,19 @@
+---
+title: DV_RProject1
+author: Brian Yeh, Valentina Chen
+output: html_document
+---
+<!-- rmarkdown v1 -->
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01 Data/Dataframes_NoNullAge.R
@@ -0,0 +1,1 @@
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where AGE is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_by2578', PASS='orcl_by2578', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
